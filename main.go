4356b160e97f9541f56f11383bc2a8592158e85d
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-06-27 18:56:32 +0100

    initialize main function and load configuration

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"github.com/CyberwizD/Task-Management-API-Endpoint/internal/config"
+	"github.com/CyberwizD/Task-Management-API-Endpoint/internal/handlers"
+	"github.com/CyberwizD/Task-Management-API-Endpoint/internal/middleware"
+	"github.com/CyberwizD/Task-Management-API-Endpoint/internal/repository"
+	"github.com/CyberwizD/Task-Management-API-Endpoint/internal/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Load configuration
+	cfg := config.Load()
+}
