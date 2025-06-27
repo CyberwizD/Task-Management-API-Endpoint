43907079ef00e6defe88c20e8c0ffe59c2f9f9fd
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-06-27 18:49:23 +0100

    add setupRoutes function and define API v1 task routes

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,1 +1,23 @@
 package api
+
+import (
+	"github.com/CyberwizD/Task-Management-API-Endpoint/internal/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRoutes(router *gin.Engine, taskHandler *handlers.TaskHandler) {
+	// Health check
+	router.GET("/health", handlers.HealthCheck)
+
+	// API v1 routes
+	v1 := router.Group("/api/v1")
+	{
+		tasks := v1.Group("/tasks")
+		{
+			tasks.POST("", taskHandler.CreateTask)
+			tasks.GET("", taskHandler.GetAllTasks)
+			tasks.GET("/:id", taskHandler.GetTaskByID)
+		}
+	}
+}
